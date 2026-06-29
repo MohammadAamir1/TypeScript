420f477cadd0639c0cf14970b951d7413946e8fd
mohdamircode <amir113918amir@gmail.com>
2026-06-29 04:12:54 -0700

    types and union

---
+++ src/02_typesInTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+// cups = "ginger";
+
+let channelName = "chaiaurcolddrink"
+
+let chaiFlavour:string = "masala tea"
+chaiFlavour = "Ginger tea"
+
+let chaiOrder:boolean = true
